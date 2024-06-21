9219a97d619ff5ba87d92ef75896c4498768bcb1
miro <jarbasai@mailfence.com>
2024-06-21 20:02:42 +0100

    automations + standardize setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from os import walk, path
 
 
 # update this!
-URL = "https://github.com/forslund/skill-spotify"
-SKILL_CLAZZ = "SkillSpotify"  # needs to match __init__.py class name
-PYPI_NAME = "skill-spotify"  # pip install PYPI_NAME
+URL = "https://github.com/OpenVoiceOS/ovos-skill-spotify"
+SKILL_CLAZZ = "SpotifySkill"  # needs to match __init__.py class name
+PYPI_NAME = "ovos-skill-spotify"  # pip install PYPI_NAME
 
-
-SKILL_AUTHOR = "forslund"
-SKILL_NAME = "skill-spotify"
+# below derived from github url to ensure standard skill_id
+SKILL_AUTHOR, SKILL_NAME = URL.split(".com/")[-1].split("/")
 SKILL_PKG = SKILL_NAME.lower().replace('-', '_')
 PLUGIN_ENTRY_POINT = (f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}'
                       f'={SKILL_PKG}:{SKILL_CLAZZ}')
@@ -36,24 +35,47 @@ def find_resource_files():
     base_dir = path.dirname(__file__)
     package_data = ["*.json"]
     for res in resource_base_dirs:
-        print(path.join(base_dir, res))
         if path.isdir(path.join(base_dir, res)):
             for (directory, _, files) in walk(path.join(base_dir, res)):
                 if files:
                     package_data.append(
                         path.join(directory.replace(base_dir, "").lstrip('/'),
                                   '*'))
-    print(package_data)
     return package_data
+
+
+def get_version():
+    """ Find the version of this skill"""
+    version_file = os.path.join(os.path.dirname(__file__), 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if int(alpha):
+        version += f"a{alpha}"
+    return version
 
 
 setup(
     # this is the package name that goes on pip
     name=PYPI_NAME,
-    version="0.1.1",
+    version=get_version(),
     url=URL,
     license='Apache-2.0',
-    package_data={SKILL_PKG: ['locale/**/*']},
+    package_dir={SKILL_PKG: ""},
+    package_data={SKILL_PKG: find_resource_files()},
     packages=[SKILL_PKG],
     include_package_data=True,
     install_requires=get_requirements("requirements.txt"),
